e2dcc579c796be354be1ea48c2e8f4744da6581f
Ingo Blechschmidt <iblech@web.de>
2015-09-23 13:06:41 +0200

    Als Archiv, um die Sprachstatistik von Github nicht zu verwirren

--- sonstiges/evaluation/Evaluations-Auswertung - Digicampus-Dateien/shadowedbox.js
+++
@@ -1,9 +0,0 @@
-
-jQuery(document).ready(function()
-{
-   
-    jQuery("ul#tabs2~*:not(.studipfooter):not(:empty)"). addClass("shadowed_box");
-    jQuery(".index_box").addClass("shadowed_box");
-    $(".blank").first().addClass("shadowed_box");
-});
-
